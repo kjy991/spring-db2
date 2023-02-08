094a876fc53f048b8e18cdae554cf1193c7c916c
엽킹 <kim91star2@naver.com>
2023-02-08 22:48:56 +0900

    스프링 트랜잭션 이해 프로젝트 생성

---
+++ src/main/java/com/transaction/springdb2/SpringDb2Application.java
@@ -0,0 +1,13 @@
+package com.transaction.springdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDb2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/transaction/springdb2/SpringDb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transaction.springdb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDb2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
